9f0e6bc7dc96b0512c7db1795b47f2795b20110f
Alejandro Bermúdez <alejo0395@gmail.com>
2021-04-03 22:53:37 -0500

    Added own lib

--- src/components/App/index.tsx
+++ src/components/App/index.tsx
@@ -1,9 +1,18 @@
+import { sum } from 'tsdx-test';
+
 import { withContextProvider } from 'contexts/UserContext';
 import Routes from 'components/Routes';
+
 import 'scss/application.scss';
 
 function App() {
-  return <Routes />;
+  const result = sum(1, 2);
+  return (
+    <>
+      {result}
+      <Routes />
+    </>
+  );
 }
 
 export default withContextProvider(App);
